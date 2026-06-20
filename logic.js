eef4150ba6073ee9fb4e192ba4b5bd95305a2f8c
Smitrodge <smitrodage8@gmail.com>
2026-06-20 11:38:18 +0530

    Ready to show

--- logic.js
+++ logic.js
@@ -3,25 +3,27 @@ let btn = document.querySelectorAll("button");
 let screen = document.querySelector("#input");
 btn.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if(btn.innerHTML == '='){
-            console.log("eue");
-            screen.placeholder= eval(str);
+        if (btn.innerHTML == '=') {
+
+            try {
+                screen.placeholder = eval(str);
+            }
+            catch (error) {
+                screen.placeholder = "Error";
+            }
         }
 
-        else if(btn.innerHTML == 'AC'){
-            console.log("clear");
-            str="0";
+        else if (btn.innerHTML == 'AC') {
+            str = "0";
             screen.placeholder = str;
         }
-         
-        else if(btn.innerHTML == 'DL'){
-            console.log("delete");
-            str = str.slice(0,str.length - 1);
+
+        else if (btn.innerHTML == 'DL') {
+            str = str.slice(0, str.length - 1);
             screen.placeholder = str;
         }
-        else{
-            console.log("else");
-            str += btn.innerHTML
+        else {
+            str += btn.innerHTML;
             screen.placeholder = str;
         }
     })
